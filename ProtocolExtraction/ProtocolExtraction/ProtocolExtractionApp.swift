f68cb2a8c86a0c1d512c84576c9d14cbbe3c809c
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-15 01:58:51 +0330

    initial xcode project setup.

---
+++ ProtocolExtraction/ProtocolExtraction/ProtocolExtractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolExtractionApp.swift
+//  ProtocolExtraction
+//
+//  Created by Mohamad Rahmani on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolExtractionApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
